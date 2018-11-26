75a1dd0fe4d35ba96c19f8b30759800e1a4fe1ca
Michael Irwin <mike.irwin1@gmail.com>
2018-11-26 18:20:55 +0000

    library book info retrieved

---
+++ library.rb
@@ -0,0 +1,25 @@
+class Library
+  attr_reader :books
+
+  def initialize()
+    @books =
+    [
+      {
+      title: "lord_of_the_rings",
+      rental_details: {
+       student_name: "Jeff",
+       date: "01/12/16"
+        }
+      }
+    ]
+  end
+
+  def book_get(name)
+    for book in @books
+      if book[:title] == name
+        return book
+      end
+    end
+  end
+
+end
--- specs/library_spec.rb
+++ specs/library_spec.rb
@@ -3,5 +3,18 @@ require('minitest/rg')
 require_relative('../library')
 
 class TestLibrary < MiniTest::Test
-  def 
+
+  def test_library
+    library = Library.new()
+  end
+
+  def test_books
+    library = Library.new()
+    assert_equal([{title: "lord_of_the_rings",rental_details: {student_name: "Jeff",date: "01/12/16"}}], library.books)
+  end
+
+  def test_book_get
+    library = Library.new()
+    assert_equal({title: "lord_of_the_rings", rental_details: {student_name: "Jeff",date: "01/12/16"}}, library.book_get('lord_of_the_rings'))
+  end
 end
