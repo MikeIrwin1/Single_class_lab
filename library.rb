5c8f25f4a3ab88401e540a86584dc18ab467437e
Michael Irwin <mike.irwin1@gmail.com>
2018-11-27 10:34:22 +0000

    refactored library_spec file with def setup

--- library.rb
+++ library.rb
@@ -1,17 +1,8 @@
 class Library
   attr_reader :books
 
-  def initialize()
-    @books =
-    [
-      {
-      title: "lord_of_the_rings",
-      rental_details: {
-       student_name: "Jeff",
-       date: "01/12/16"
-        }
-      }
-    ]
+  def initialize(books)
+    @books = books
   end
 
   def book_get(name)
--- specs/library_spec.rb
+++ specs/library_spec.rb
@@ -6,25 +6,22 @@ class TestLibrary < MiniTest::Test
   def setup
     @books = [{title: "lord_of_the_rings",rental_details: {student_name: "Jeff",date: "01/12/16"}}]
 
-  end
-
-  def test_library
-    library = Library.new()
+    @library = Library.new(@books)
   end
 
   def test_books
-    library = Library.new()
-    assert_equal(@books, library.books)
+    book_list = @library.books()
+    assert_equal(@books, book_list)
   end
 
   def test_book_get
-    library = Library.new()
-    assert_equal({title: "lord_of_the_rings", rental_details: {student_name: "Jeff",date: "01/12/16"}}, library.book_get('lord_of_the_rings'))
+    found_book = @library.book_get("lord_of_the_rings")
+    assert_equal({title: "lord_of_the_rings", rental_details: {student_name: "Jeff",date: "01/12/16"}}, found_book)
   end
 
   def test_book_details
-    library = Library.new()
-    assert_equal({student_name: "Jeff", date: "01/12/16"}, library.book_details('lord_of_the_rings'))
+    book_details = @library.book_details("lord_of_the_rings")
+    assert_equal({student_name: "Jeff", date: "01/12/16"}, book_details)
   end
 
   def test_add_book
@@ -34,16 +31,14 @@ class TestLibrary < MiniTest::Test
         date: ''
       }
     }
-    library = Library.new()
-    library.add_book('trainspotting')
-    assert_equal(book,library.book_get('trainspotting'))
+    added_book = @library.add_book('trainspotting')
+    assert_equal(book, added_book[1])
   end
 
   def test_change_details
     rental = {title: 'lord_of_the_rings', rental_details: {student_name: 'Chris', date: '25/12/2018'}}
-    library = Library.new()
 
-    new_rental = library.change_details('lord_of_the_rings', 'Chris', '25/12/2018')
+    new_rental = @library.change_details('lord_of_the_rings', 'Chris', '25/12/2018')
 
     assert_equal(rental, new_rental)
   end
